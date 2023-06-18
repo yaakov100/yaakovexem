b09e373107e00621bc72859ec8e9bcc14f28e395
DvoraRit <DvoraRit@DESKTOP-NF9HR1T>
2023-06-18 09:52:15 +0300

    Add project files.

---
+++ testGit/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testGit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("קובי תודה רבה  על הלימוד ההעשרת לכולם את הידע בצורה טובה וחויתית" +
+                "+" "אשמח לקבל ציון 100 );
+        }
+    }
+}
